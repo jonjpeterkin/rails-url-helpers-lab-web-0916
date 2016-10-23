a7de2bc7ae643607bfc6e5699f1ede5c16a88d34
stompdoctor <jonjpeterkin@gmail.com>
2016-10-23 01:46:00 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,13 @@ class StudentsController < ApplicationController
   end
 
   def show
+    set_student
+  end
+
+  def active
+    set_student
+    @student.toggle_active
+    redirect_to student_path(@student)
   end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,16 @@
 class Student < ActiveRecord::Base
+	after_initialize :init
+
+	def init
+		self.active = false if self.active.nil? && self.has_attribute?(:active)
+	end
+
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+  def toggle_active
+  	self.active = !self.active
+  	save
   end
 end
